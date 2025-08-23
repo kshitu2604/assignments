c134c40336221e52df6a5e53f3224d4258a68acb
kshitija <kshituraut2604@gmail.com>
2025-08-23 18:48:41 +0530

    add1

--- src/assignments/Assignment11_Diamondpattern.java
+++ src/assignments/Assignment11_Diamondpattern.java
@@ -18,8 +18,8 @@ public class Assignment11_Diamondpattern {
 			System.out.println();
 		}
 		for (int i = (n-1);i >=1 ; i--) {
-			for (int j = (n-1) ; j <= (n-i); j--) {
-				System.out.print("  ");	
+			for (int j = i; j < n; j++) {
+				System.out.print(" ");	
 			}
 			for(int k =1; k <= i ;k++) {
 				System.out.print(k + " ");
